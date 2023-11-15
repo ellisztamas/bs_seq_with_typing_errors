8cb1df058645e98070324aecad5947eb57f280e7
Thomas Ellis <thomas.ellis@gmi.oeaw.ac.at>
2023-11-15 14:43:44 +0100

    Methylation state with Python
    I swapped the scripts to call methylation state from a dodgy old implementation in R to the Python implementation in epiclinestools.
    
     On branch main
     Your branch is ahead of 'origin/main' by 2 commits.
     Changes to be committed:
    	deleted:    03_analysis/04_methylation_status/03_lik_methylation_state.R
    	new file:   03_analysis/04_methylation_status/03_lik_methylation_state.py
    	deleted:    03_analysis/04_methylation_status/04_estimate_beta_parameters.R
    	new file:   03_analysis/04_methylation_status/04_plot_methylation_state.R
    	deleted:    03_analysis/04_methylation_status/05_plot_methylation_state.R
    	modified:   04_manuscript/figure4.eps

---
+++ 03_analysis/04_methylation_status/03_lik_methylation_state.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import epiclinestools as epi
+
+# Import counts of converted and unconverted reads across the chloroplasts, on
+# genes and on CMT2- and RdDM-targetted TEs.
+chloroplast = pd.read_csv(
+    "03_analysis/02_across_chloroplast/output/Col0_05_13X.windows.csv"
+)
+chloroplast['id'] = chloroplast['start'].astype(str)
+chloroplast = chloroplast.loc[chloroplast['start'] <= 75000]
+genes = pd.read_csv("03_analysis/04_methylation_status/output/mC_genes.csv")
+cmt2 = pd.read_csv("03_analysis/04_methylation_status/output/mC_on_cmt2_TEs.csv")
+rddm = pd.read_csv("03_analysis/04_methylation_status/output/mC_on_rddm_TEs.csv")
+
+# Estimate shape parameters of the beta distribution
+# Estimate the proportion of unconverted reads
+chloroplast['n'] = chloroplast['unconverted'] + chloroplast['converted']
+chloroplast['theta'] = chloroplast['unconverted'] / chloroplast['n']
+# Use the mean and variance across the chloroplast to estimate beta shape parameters
+mu = chloroplast['theta'].loc[chloroplast['context'] == "total"].mean()
+sigma = chloroplast['theta'].loc[chloroplast['context'] == "total"].var()
+ab_errors = epi.estimate_beta_parameters(mu, sigma)
+
+meth_states = pd.concat([
+    epi.methylation_state(chloroplast, ab_errors, hard_calls=True).assign(type='Chloroplast'),
+    epi.methylation_state(genes, ab_errors, hard_calls=True).assign(type='Genes'),
+    epi.methylation_state(rddm, ab_errors, hard_calls=True).assign(type='RdDM TEs'),
+    epi.methylation_state(cmt2, ab_errors, hard_calls=True).assign(type='CMT2 TEs')
+])
+
+meth_states.to_csv("03_analysis/04_methylation_status/output/meth_states.csv")
---
+++ 03_analysis/04_methylation_status/04_plot_methylation_state.R
@@ -0,0 +1,48 @@
+#' Script to plot likelihoods of methylation state across the chloroplast, on
+#' genes, CMT2- and RDdM-targetted TEs.
+#'
+#' This is based on counts of methylated and unmethylated reads calculated with
+#' the scripts `01_mC_genes_TEs.py` and `02_submit_mC_genes_TEs.sh`.
+#'
+#' Tom Ellis, 10th November 2023
+
+library(tidyverse)
+
+meth_states <- read_csv("03_analysis/04_methylation_status/output/meth_states.csv") %>%
+  rename(state = call) %>%
+  filter(coverage >= 10) %>%
+  mutate(
+    type = fct_relevel(factor(type), c("Chloroplast", "Genes")),
+    state = case_match(
+      state,
+      "unmethylated" ~ "Unmethylated",
+      "CG-only" ~ "GBM-like",
+      "TE-like" ~ "TE-like"
+    )
+  )
+
+meth_states %>%
+  ggplot(aes(x = state, y = ..prop.., group=1)) +
+  geom_bar(stat = "count") +
+  labs(
+    x = "Methylation state",
+    y = "Frequency"
+  ) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  ) +
+  facet_grid(~ type)
+
+ggsave(
+  filename = "04_manuscript/figure4.eps",
+  device="eps",
+  units = "cm", width = 16.9, height = 8
+)
+
+# meth_states %>%
+#   pivot_longer(unmethylated : te_like, names_to = "State", values_to = "Probability") %>%
+#   ggplot(aes( x = State, y = Probability)) +
+#   geom_boxplot() +
+#   theme_bw() +
+#   facet_grid(~type)
